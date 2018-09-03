123be5238f85e7e1874c7127afe30939cfd850fd
Cody Loyd <codyloyd@gmail.com>
2018-09-03 09:44:07 -0500

    add markers to board

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import "phaser";
 import { SimpleScene } from "./scenes/simple-scene";
 
 const gameConfig = {
-  width: 356,
-  height: 356,
+  width: 256,
+  height: 256,
   pixelArt: true,
-  zoom: 2,
+  zoom: 3,
   scene: SimpleScene
 };
 
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -10,11 +10,22 @@ export class SimpleScene extends Phaser.Scene {
     this.load.image("marker", "assets/art/musicgame_4.png");
   }
   create() {
-    this.drawBoard();
-    this.cursors = this.input.keyboard.createCursorKeys();
+    this.drawBoard({});
+    this.markerGroup = new Phaser.GameObjects.Group(this);
+    this.markers = {};
+    this.add.image(32, 32, "marker").setOrigin(0, 0);
     this.player = new Player({
-      sprite: this.add.image(0, 0, "dude").setOrigin(0, 0)
+      x: 0,
+      y: 7,
+      sprite: this.add
+        .image(0, 0, "dude")
+        .setOrigin(0, 0)
+        .setDepth(10)
     });
+    // keys
+    this.keys = {};
+    this.keys.x = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
   update() {
     if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
@@ -29,24 +40,35 @@ export class SimpleScene extends Phaser.Scene {
     if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
       this.player.move({ x: -1 });
     }
+    if (Phaser.Input.Keyboard.JustDown(this.keys.x)) {
+      this.addMarker({ x: this.player.x, y: this.player.y });
+    }
   }
 
-  drawBoard() {
-    for (let x = 0; x < 18; x++) {
-      for (let y = 0; y < 18; y++) {
-        if (x == 0 && y > 0 && y < 17) {
+  addMarker({ x = 0, y = 0 }) {
+    this.markers[`${x},${y}`] = this.add
+      .image(x * 16 + 16, y * 16 + 16, "marker")
+      .setOrigin(0, 0)
+      .setDepth(1);
+    console.log(this.markers);
+  }
+
+  drawBoard({ xSize = 8, ySize = 8 }) {
+    for (let x = 0; x < xSize + 2; x++) {
+      for (let y = 0; y < ySize + 2; y++) {
+        if (x == 0 && y > 0 && y < ySize + 1) {
           this.add.image(x * 16, y * 16, "tile_edge").setOrigin(0, 0);
-        } else if (x == 17 && y > 0 && y < 17) {
+        } else if (x == xSize + 1 && y > 0 && y < ySize + 1) {
           this.add
             .image(x * 16, y * 16, "tile_edge")
             .setOrigin(1, 1)
             .setAngle(180);
-        } else if (y == 0 && x > 0 && x < 17) {
+        } else if (y == 0 && x > 0 && x < xSize + 1) {
           this.add
             .image(x * 16 + 16, y * 16, "tile_edge")
             .setOrigin(0, 0)
             .setAngle(90);
-        } else if (y == 17 && x > 0 && x < 17) {
+        } else if (y == ySize + 1 && x > 0 && x < xSize + 1) {
           this.add
             .image(x * 16, y * 16 + 16, "tile_edge")
             .setOrigin(0, 0)
@@ -56,17 +78,17 @@ export class SimpleScene extends Phaser.Scene {
             .image(x * 16, y * 16, "tile_corner")
             .setOrigin(0, 0)
             .setAngle(0);
-        } else if (x == 0 && y == 17) {
+        } else if (x == 0 && y == ySize + 1) {
           this.add
             .image(x * 16, y * 16 + 16, "tile_corner")
             .setOrigin(0, 0)
             .setAngle(270);
-        } else if (x == 17 && y == 17) {
+        } else if (x == xSize + 1 && y == ySize + 1) {
           this.add
             .image(x * 16, y * 16, "tile_corner")
             .setOrigin(1, 1)
             .setAngle(180);
-        } else if (x == 17 && y == 0) {
+        } else if (x == xSize + 1 && y == 0) {
           this.add
             .image(x * 16 + 16, y * 16, "tile_corner")
             .setOrigin(0, 0)
